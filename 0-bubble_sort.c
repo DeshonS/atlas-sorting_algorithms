95f0aebbe5b8101aa158c5705f2f968fc8229a87
DeshonS <deshonjewell@gmail.com>
2024-08-08 16:09:27 -0500

    finished task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+    int i, j;
+    int temp;
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
