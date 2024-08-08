e5d34da06c4a601d44ffee18065745e2a19ee8b0
DeshonS <deshonjewell@gmail.com>
2024-08-08 16:52:01 -0500

    formatted

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,44 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - opening of file
+ * @list: double pointer to list
+ * Return: None (void)
+ */
+
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *new, *last;
+listint_t *new, *last;
 
-    if (list == NULL)
-    {
-        return;
-    }
-    new = (*list)->next;
-    while (new)
-    {
-        last = new->prev;
-        while (last && new->n < last->n)
-        {
-            if(last->prev)
-            {
-                last->prev->next = new;
-            }
-            else
-            {
-                *list = new;
-            }
-            if (new->next)
-            {
-                new->next->prev = last;
-            }
-            last->next = new->next;
-            new->prev = last->prev;
-            new->next = last;
-            last->prev = new;
-            print_list(*list);
-            last = new->prev;
-        }
-        new = new->next;
-    }
+if (list == NULL)
+{
+return;
+}
+new = (*list)->next;
+while (new)
+{
+last = new->prev;
+while (last && new->n < last->n)
+{
+if(last->prev)
+{
+last->prev->next = new;
+}
+else
+{
+*list = new;
+}
+if (new->next)
+{
+new->next->prev = last;
+}
+last->next = new->next;
+new->prev = last->prev;
+new->next = last;
+last->prev = new;
+print_list(*list);
+last = new->prev;
+}
+new = new->next;
+}
 }
