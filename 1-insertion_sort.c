bd62488c6505c07c4cf7e25a91200ee2f82969e9
DeshonS <deshonjewell@gmail.com>
2024-08-08 16:47:31 -0500

    task 1 done

---
+++ 1-insertion_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *new, *last;
+
+    if (list == NULL)
+    {
+        return (NULL);
+    }
+    new = (*list)->next;
+    while (new)
+    {
+        last = new->prev;
+        while (last && new->n < last->n)
+        {
+            if(last->prev)
+            {
+                last->prev->next = new;
+            }
+            else
+            {
+                list = new;
+            }
+            if (new->next)
+            {
+                new->next->prev = last;
+            }
+            last->next = new->next;
+            new->prev = last->prev;
+            new->next = last;
+            last->prev = new;
+            print_list(*list);
+            last = new->prev;
+        }
+        new = new->next;
+    }
+}
