e87c463f0a88b8c1df86a9c7bcdbece7bc2ab475
DeshonS <deshonjewell@gmail.com>
2024-08-08 16:25:16 -0500

    formatted file for betty and added description

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,27 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - opening of file
+ * @array: pointer to the array
+ * @size: size of the array
+ * Return: None (void)
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-    long unsigned int i, j;
-    int temp;
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+long unsigned int i, j;
+int temp;
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
 }
-
+}
+}
+}
