4092a399f50d541928b9655c33b9761d69e40926
DeshonS <deshonjewell@gmail.com>
2024-08-09 17:23:18 -0500

    formatted, added description, and changed int to size_t for comparison error fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,24 +1,32 @@
 #include "sort.h"
 
+/**
+ * selection_sort - opening of file
+ * @array: pointer to the array
+ * @size: size of the array
+ * Return: none (void)
+ */
+
 void selection_sort(int *array, size_t size)
 {
-    int i, j, min, temp;
+size_t i, j, min, temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-            {
-                min = j;
-            }
-            if (min != i)
-            {
-                temp = array[min];
-                array[min] = array[i];
-                array[i] = temp;
-            }
-        }
-    }
+for (i = 0; i < size - 1; i++)
+{
+min = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min])
+{
+min = j;
+}
+if (min != i)
+{
+temp = array[min];
+array[min] = array[i];
+array[i] = temp;
+print_array(array, size);
+}
+}
+}
 }
