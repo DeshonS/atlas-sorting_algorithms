6120dea990560e1f44de2973c4ae0b9eb92e3e63
DeshonS <deshonjewell@gmail.com>
2024-08-10 06:07:18 -0500

    changed an if statement on line 18 to fix duplicate error

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,13 +13,9 @@ int part(int *array, int low, int high, size_t size)
 int pivot = array[high];
 int i = low - 1;
 int t, j;
-if (low == high)
-{
-    return i + 1;
-}
 for (j = low; j <= high - 1; j++)
 {
-if (array[j] < pivot)
+if (array[j] <= pivot)
 {
 i++;
 if (i != j)
